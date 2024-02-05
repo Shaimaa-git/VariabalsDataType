705a25cafbce71000930a4b5c5330a60f0efe025
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-05 12:34:25 +0400

    String data type

---
+++ StringDataType.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String name="Shaima Al Maqbali";
+        String city="Sohar";
+        System.out.println(name+":"+city);
+    }
+}
