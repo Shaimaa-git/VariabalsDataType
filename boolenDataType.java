8f9869c9b5d5c8d9deb40a21555a57bd42e94f00
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-05 13:25:50 +0400

    boolenDataType

---
+++ boolenDataType.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+       boolean flag1=true;
+       boolean flag2=false;
+        System.out.println(flag1);
+        System.out.println(flag2);
+       
+    }
+}
